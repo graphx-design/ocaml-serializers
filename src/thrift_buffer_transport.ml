86cde969835ddff1235684988e08d23737850684
Stéphane Lavergne <lis@imars.com>
2019-07-31 07:20:37 -0400

    Update to ocamlformat 0.10

--- src/thrift_buffer_transport.ml
+++ src/thrift_buffer_transport.ml
@@ -20,21 +20,22 @@ class t () =
     method close = opened <- false
 
     method read buf off len =
-      if opened
-      then (
+      if opened then (
         let to_copy = min len (String.length buffer - read_offset) in
-        if to_copy > 0
-        then (
+        if to_copy > 0 then (
           String.blit buffer read_offset buf off to_copy;
           read_offset <- read_offset + to_copy;
-          to_copy)
-        else 0)
-      else raise (T.E (T.NOT_OPEN, "TBufferTransport: Channel was closed"))
+          to_copy
+        ) else
+          0
+      ) else
+        raise (T.E (T.NOT_OPEN, "TBufferTransport: Channel was closed"))
 
     method write buf off len =
-      if opened
-      then buffer <- buffer ^ String.sub buf off len
-      else raise (T.E (T.NOT_OPEN, "TBufferTransport: Channel was closed"))
+      if opened then
+        buffer <- buffer ^ String.sub buf off len
+      else
+        raise (T.E (T.NOT_OPEN, "TBufferTransport: Channel was closed"))
 
     method flush =
       buffer <- "";
